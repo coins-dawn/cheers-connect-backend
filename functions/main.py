796f51e8f2f87b0fa73f8f38e0a37864c4714b11
igarashi339 <igarashi339@gmail.com>
2023-10-31 20:59:55 +0900

    Revert "ステータスコードを設定"
    This reverts commit d749de6371155ae9068cf1c5034fa3945b6d5099.

--- functions/main.py
+++ functions/main.py
@@ -68,12 +68,8 @@ def recommend_store_by_station(
 
 @https_fn.on_request(
     cors=options.CorsOptions(
-        cors_origins=[
-            r"https://cheers-connect-e3c35.web.app",
-            r"http://localhost:3000",
-            r"https://cheers-connect-e3c35--.*-.*.web.app",
-        ],
-        cors_methods=["get", "post"],
+        cors_origins=[r"https://cheers-connect-e3c35.web.app", r"http://localhost:3000", r"https://cheers-connect-e3c35--.*-.*.web.app"],
+        cors_methods=["get", "post"]
     )
 )
 def execute(req: https_fn.Request) -> https_fn.Response:
@@ -81,21 +77,21 @@ def execute(req: https_fn.Request) -> https_fn.Response:
     station_detail_list, store_detail_list, station_store_distance = road_data()
 
     if "station_id" not in req_param_dict:
-        return https_fn.Response("error: station_idが指定されていません。", status=400)
+        return https_fn.Response("error: station_idが指定されていません。")
     station_id_str = req_param_dict["station_id"]
 
     if not station_detail_list.is_exist_id(station_id_str):
-        return https_fn.Response("error: 存在しないstation_idが指定されています。", status=400)
+        return https_fn.Response("error: 存在しないstation_idが指定されています。")
 
     if "search_radius" not in req_param_dict:
-        return https_fn.Response("error: search_radiusが指定されていません。", status=400)
+        return https_fn.Response("error: search_radiusが指定されていません。")
     search_radius_str = req_param_dict["search_radius"]
     if not search_radius_str.isdecimal():
-        return https_fn.Response("error: search_radiusが数値ではありません。", status=400)
+        return https_fn.Response("error: search_radiusが数値ではありません。")
     search_radius = int(search_radius_str)
     if search_radius > MAX_SEARCH_RADIUS_M:
         return https_fn.Response(
-            f"error: search_radiusが最大値({MAX_SEARCH_RADIUS_M})を超えています。", status=400
+            f"error: search_radiusが最大値({MAX_SEARCH_RADIUS_M})を超えています。"
         )
 
     recommend_store_list = recommend_store_by_station(
@@ -112,6 +108,5 @@ def execute(req: https_fn.Request) -> https_fn.Response:
             },
             ensure_ascii=False,
             indent=2,
-        ),
-        status=200,
+        )
     )
